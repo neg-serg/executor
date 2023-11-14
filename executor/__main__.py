2c44a1dffd2e99ceac8aa308812a59584b683576
Sergey Miroshnichenko <serg.zorg@gmail.com>
2023-11-14 03:08:12 +0300

    [executor] stash autoreload(nonworking yet)

--- executor/__main__.py
+++ executor/__main__.py
@@ -1,24 +1,23 @@
 #!/usr/bin/python
 import asyncio
 from sys import argv
-import subprocess
 from executor.executor import Executor, MsgBroker
 
-if __name__ == '__main__':
-    loop=asyncio.new_event_loop()
+def main(loop):
     executor = Executor()
     cmd = ''
     if len(argv) >= 2: # should have cmd at least
         cmd = argv[1]
         if cmd == 'daemon':
             port = 15556
-            subprocess.Popen([
-                'watchexec', '-w', executor.config.dir(),
-                'echo reload | nc localhost 15556 -N'
-            ], stdout=subprocess.PIPE)
             MsgBroker.mainloop(loop, executor, port)
     for arg in argv[2:]:
-        if cmd == 'config' or cmd == 'cfg':
+        if cmd in {'config', 'cfg'}:
             print(executor.cfg[arg])
-        if cmd == 'run' or cmd == 'prog':
+        elif cmd in {'run', 'prog'}:
             executor.run(arg)
+
+if __name__ == '__main__':
+    loop=asyncio.new_event_loop()
+    main(loop)
+    loop.close()
