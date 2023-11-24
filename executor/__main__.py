d9879403939156f97424778a20a31f57a37bbe5c
Sergey Miroshnichenko <serg.zorg@gmail.com>
2023-11-25 01:38:27 +0300

    Revert "[executor] stash autoreload(nonworking yet)"
    This reverts commit 2c44a1dffd2e99ceac8aa308812a59584b683576.

--- executor/__main__.py
+++ executor/__main__.py
@@ -1,23 +1,24 @@
 #!/usr/bin/python
 import asyncio
 from sys import argv
+import subprocess
 from executor.executor import Executor, MsgBroker
 
-def main(loop):
+if __name__ == '__main__':
+    loop=asyncio.new_event_loop()
     executor = Executor()
     cmd = ''
     if len(argv) >= 2: # should have cmd at least
         cmd = argv[1]
         if cmd == 'daemon':
             port = 15556
+            subprocess.Popen([
+                'watchexec', '-w', executor.config.dir(),
+                'echo reload | nc localhost 15556 -N'
+            ], stdout=subprocess.PIPE)
             MsgBroker.mainloop(loop, executor, port)
     for arg in argv[2:]:
-        if cmd in {'config', 'cfg'}:
+        if cmd == 'config' or cmd == 'cfg':
             print(executor.cfg[arg])
-        elif cmd in {'run', 'prog'}:
+        if cmd == 'run' or cmd == 'prog':
             executor.run(arg)
-
-if __name__ == '__main__':
-    loop=asyncio.new_event_loop()
-    main(loop)
-    loop.close()
